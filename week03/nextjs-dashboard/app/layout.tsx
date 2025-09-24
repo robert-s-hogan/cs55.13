36be520496124a0ec42c6141bd8ee391643d6c80
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-09-24 13:58:13 -0700

    added week3 assignment 4. first commit

---
+++ week03/nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,14 @@
+// imports tailwind css as global styles
+import "@/app/ui/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
